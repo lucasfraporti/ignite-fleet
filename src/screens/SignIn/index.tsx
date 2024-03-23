31f8d886a4e2230fbbca9a1dff0aba9740f5d5d8
Lucas Fraporti <80408351+lucasfraporti@users.noreply.github.com>
2024-03-23 16:38:34 -0300

    setting up authentication with Google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,55 @@
+import { useState } from 'react'
+import { Alert } from 'react-native'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+
 import { Container, Title, Slogan } from './styles'
 
 import backgroundImg from '../../assets/background.png'
 
 import { Button } from '../../components/Button'
 
+GoogleSignin.configure({
+  scopes: ['profile', 'email'],
+  webClientId: process.env.WEB_CLIENT_ID,
+  iosClientId: process.env.IOS_CLIENT_ID,
+})
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true)
+      const { idToken } = await GoogleSignin.signIn()
+
+      if (idToken) {
+        console.log(idToken)
+      } else {
+        Alert.alert(
+          'Falha ao autenticar',
+          'Não foi possível autenticar-se com a sua conta Google.',
+        )
+      }
+    } catch (error) {
+      console.error(error)
+      setIsAuthenticating(false)
+      Alert.alert(
+        'Falha ao autenticar',
+        'Não foi possível autenticar-se com a sua conta Google.',
+      )
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   )
 }
