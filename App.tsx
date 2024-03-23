860a5225aa5eb98a6dafd91a0d24b4e99893ad89
Lucas Fraporti <80408351+lucasfraporti@users.noreply.github.com>
2024-03-23 14:43:06 -0300

    configuring to use env

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 import {
   useFonts,
@@ -9,7 +10,6 @@ import theme from './src/theme'
 
 import { SignIn } from './src/screens/SignIn'
 import { Loading } from './src/components/Loading'
-import { StatusBar } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
