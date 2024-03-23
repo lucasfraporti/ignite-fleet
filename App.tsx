a61c8abe9e7b4788b5cb21b7ecb5c8e2f2dbb563
Lucas Fraporti <80408351+lucasfraporti@users.noreply.github.com>
2024-03-23 13:06:20 -0300

    project configuration and signin page created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import theme from './src/theme'
+
+import { SignIn } from './src/screens/SignIn'
+import { Loading } from './src/components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </ThemeProvider>
+  )
+}
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import { Container, Title, Slogan } from './styles'
+
+import backgroundImg from '../../assets/background.png'
+
+import { Button } from '../../components/Button'
+
+export function SignIn() {
+  return (
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title="Entrar com Google" />
+    </Container>
+  )
+}
